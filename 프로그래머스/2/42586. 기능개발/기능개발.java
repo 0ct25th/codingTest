0f0014d5fc84d7b1adf30cd1affe0b07d125704c
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-06 21:21:16 +0900

    [level 2] Title: 기능개발, Time: 0.54 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,7 +5,7 @@ class Solution {
         Queue<Integer> dq = new ArrayDeque<>();
         List<Integer> answer = new ArrayList<>();
         
-        // 1. 배포가 필요한 날짜 queue에 담기
+        // 배포가 필요한 날짜 queue에 담기
         for (int i = 0;  i < progresses.length; i++) {
             dq.offer((100 - progresses[i]) % speeds[i] == 0 ? (100 - progresses[i]) / speeds[i] : ((100 - progresses[i]) / speeds[i]) + 1);
         }
@@ -13,6 +13,7 @@ class Solution {
         int now = dq.poll();
         int count = 1;
         
+        // dq가 비어져 있을 때까지 진행
         while(!dq.isEmpty()) {
             if (now >= dq.peek()) { // 현재 배포 날짜가 dq의 다음 기능의 배포 날짜보다 크면 추가
                 count++;
