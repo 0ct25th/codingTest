ae7b83eb7fe2e65bb861efd19f973d266d4b902f
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-07 16:56:02 +0900

    [level 2] Title: 주식가격, Time: 33.18 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,14 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer =  new int[prices.length];
+        Queue<Integer> dq = new ArrayDeque<>();
+        int index = 0;
         
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                answer[i]++;
-                
-                if(prices[i] > prices[j])
+        for(int price: prices)
+            dq.offer(price);
+        
+        while(!dq.isEmpty()) {
+            int now_price = dq.poll();
+            
+            for(int i = prices.length - dq.size(); i < prices.length; i++) {
+                // 가격이 떨어진 경우
+                if (now_price > prices[i]) {
+                    answer[index]++;
                     break;
+                }
+                
+                // 가격이 떨어지지 않은 경우
+                else
+                    answer[index]++;
             }
+            
+            index++;
         }
         
         return answer;
