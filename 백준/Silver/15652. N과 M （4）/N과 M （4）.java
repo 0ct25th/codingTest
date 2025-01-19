6fe55ffe4ad94971e045a63c1edf165a821665e8
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-19 17:09:59 +0900

    [Silver III] Title: N과 M (4), Time: 80 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,42 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N;			// 1부터 N까지
-	static int M;			// 길이가 M인 수열 저장
-	static int[] numbers;	// 완성된 수열 저장
-	
-	public static void main(String[] args) throws IOException{
+
+	static int N, M;
+	static int[] picks;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String string = br.readLine();
-		String[] strArr = string.split(" ");
-		
-		N = Integer.parseInt(strArr[0]);
-		M = Integer.parseInt(strArr[1]);
-		
-		numbers = new int[M];
-		
-		comb(0, 1);
-	}
-	
-	private static void comb(int cnt, int start) {
-		// 기저 조건
-		if (cnt == M) {
-			for (int i: numbers) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		// 모든 수 탐색하기
-		for (int i = start; i <= N; i++) {
-			numbers[cnt] = i;
-			comb(cnt + 1, i);
-		}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+		sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		picks = new int[M];
+		permutation(0, 0);
+
+		bw.write(sb.toString());
+		bw.flush();
+
+		br.close();
+		bw.close();
 	}
 
+	static void permutation(int depth, int start) {
+		// 기저조건: M개를 모두 선택한 경우
+		if (depth == M) {
+			for (int pick : picks)
+				sb.append(pick).append(" ");
+
+			sb.append("\n");
+
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+			picks[depth] = i + 1;
+			permutation(depth + 1, i);
+		}
+	}
 }
