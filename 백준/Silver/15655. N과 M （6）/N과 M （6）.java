5550a1877998d30c6e4966c25679ebd94181b0af
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-19 17:50:01 +0900

    [Silver III] Title: N과 M (6), Time: 72 ms, Memory: 11668 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static StringBuilder sb;
+
+	static int N, M;
+	static int[] numbers, picks;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+		sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		numbers = new int[N];
+		for (int i = 0; i < N; i++)
+			numbers[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(numbers);
+		picks = new int[M];
+		combination(0, 0);
+
+		bw.write(sb.toString());
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+
+	static void combination(int depth, int start) {
+		// 기저 조건: M개를 다 고른 경우
+		if (depth == M) {
+			for (int pick : picks)
+				sb.append(pick).append(" ");
+
+			sb.append("\n");
+
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+			picks[depth] = numbers[i];
+			combination(depth + 1, i + 1);
+		}
+	}
+}
