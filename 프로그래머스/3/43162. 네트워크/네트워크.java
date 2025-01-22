5b7011d5f422e6f1927dd4065a48b2cc63837ada
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-22 22:21:45 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
 class Solution {
+    
+    static int N, M;
     static boolean[] isVisited;
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
         isVisited = new boolean[n];
+        int answer = 0;
         
         for(int i = 0; i < n; i++) {
-            if(!isVisited[i]) {
-                bfs(i, n, computers);
-                answer++;
-            } 
+            if(isVisited[i])
+                continue;
+            
+            bfs(i, n, computers);
+            answer++;
         }
         
         return answer;
     }
     
-    public void bfs(int start, int n, int[][] computers) {
+    static void bfs(int start, int n, int[][] computers) {
         Queue<Integer> dq = new ArrayDeque<>();
-        dq.offer(start); // 덱 삽입
+        
+        isVisited[start] = true;
+        dq.offer(start);
         
         while(!dq.isEmpty()) {
-            int now = dq.poll();
-            isVisited[now] = true; // 방문 처리
+            int cur = dq.poll();
             
             for(int i = 0; i < n; i++) {
-                if(i == now || isVisited[i])
-                    continue;
-                
-                if(computers[now][i] == 0)
+                // 자기 자신인 경우 || 이미 방문한 경우 || 연결되어 있지 않은 경우
+                if(cur == i || isVisited[i] || computers[cur][i] == 0)
                     continue;
                 
                 isVisited[i] = true;
