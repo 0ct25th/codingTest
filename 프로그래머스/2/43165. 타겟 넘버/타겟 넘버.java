480e8f2690593fa9d200886469619dbfd708f6cb
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-22 21:52:29 +0900

    [level 2] Title: 타겟 넘버, Time: 7.11 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,27 @@
 class Solution {
+    
     static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        subset(0, 0, target, numbers);
+        
+        dfs(0, 0, numbers, target);
+        
         return answer;
     }
     
-    public void subset(int depth, int sum, int target, int[] numbers) {
-        // 기저조건
+    static void dfs(int depth, int sum, int[] numbers, int target) {
+        // 기저조건: 모든 숫자가 사용 된 경우
         if(depth == numbers.length) {
-            if(sum == target) answer++;
+            if(sum == target)
+                answer++;
             
             return;
         }
         
         // 더하기
-        subset(depth + 1, sum + numbers[depth], target, numbers);
+        dfs(depth + 1, sum + numbers[depth], numbers, target);
         
         // 빼기
-        subset(depth + 1, sum - numbers[depth], target, numbers);
+        dfs(depth + 1, sum - numbers[depth], numbers, target);
     }
 }
