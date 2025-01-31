2a865d91934551690d33d3bd878774b51f25313f
yoon <72852508+0ct25th@users.noreply.github.com>
2025-01-31 17:30:08 +0900

    [Silver II] Title: 싸이버개강총회, Time: 412 ms, Memory: 62928 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static String S, E, Q;
+	static int sh, sm, eh, em, qh, qm, result;
+	static Set<String> set;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		// 첫번째 줄에는 개강총회를 시작한 시간 S, 개강총회를 끝낸 시간 E, 개강총회 스트리밍을 끝낸 시간 Q가 주어진다.
+		st = new StringTokenizer(br.readLine());
+		S = st.nextToken();
+		E = st.nextToken();
+		Q = st.nextToken();
+
+		// 채팅 기록들이 시간순으로 주어지는데, (시간) (학회원 닉네임)의 형태로 주어진다.
+		String input;
+		set = new HashSet<>();
+		while ((input = br.readLine()) != null) {
+			String[] str = input.split(" ");
+			
+            // 종료 조건
+			if(str.length < 2)
+				break;
+
+			if (str[0].compareTo(S) <= 0)
+				set.add(str[1]);
+
+			else if (str[0].compareTo(E) >= 0 && str[0].compareTo(Q) <= 0) {
+				if (set.contains(str[1])) {
+					result++;
+					set.remove(str[1]);
+				}
+			}
+		}
+		System.out.println(result);
+	}
+
+}
