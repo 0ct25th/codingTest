dd743632fc0434afe1bb93a153b6949cab0299f7
yoon <72852508+0ct25th@users.noreply.github.com>
2025-02-01 15:51:41 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 492 ms, Memory: 54632 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N;
+	static Queue<Coord> pq;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+
+		pq = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			pq.offer(new Coord(x, y));
+		}
+
+		while (!pq.isEmpty()) {
+			Coord coord = pq.poll();
+
+			sb.append(coord.x).append(" ").append(coord.y).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	static class Coord implements Comparable<Coord> {
+		int x, y;
+
+		Coord(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Coord o) {
+			return this.x == o.x ? (this.y - o.y) : (this.x - o.x);
+		}
+	}
+}
