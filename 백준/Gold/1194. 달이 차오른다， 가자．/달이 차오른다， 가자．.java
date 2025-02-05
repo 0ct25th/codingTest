6a34203257ef3d90e165d79e6c73cd8fa335b981
yoon <72852508+0ct25th@users.noreply.github.com>
2025-02-05 11:16:02 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 88 ms, Memory: 13872 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,102 +1,105 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static char map[][];
-	static int sr, sc, er, ec;
-	static boolean[][][] isVisited;
+	static int N, M, sr, sc;
+	static char[][] map;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
-		// 첫째 줄에 미로의 세로 크기 N과 가로 크기 M이 주어진다. (1 ≤ N, M ≤ 50)
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		// 둘째 줄부터 N개의 줄에 미로의 모양이 주어진다.
-		// 같은 타입의 열쇠가 여러 개 있을 수 있고, 문도 마찬가지이다.
-		// 그리고, 문에 대응하는 열쇠가 없을 수도 있다. '0'은 한 개, '1'은 적어도 한 개 있다. 열쇠는 여러 번 사용할 수 있다.
-		map = new char[N][];
-		for (int i = 0; i < N; i++) {
-			map[i] = br.readLine().toCharArray();
+		map = new char[N][M];
+		for (int r = 0; r < N; r++) {
+			String str = br.readLine();
+			for (int c = 0; c < M; c++) {
+				map[r][c] = str.charAt(c);
 
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == '0') { // 민식이의 현재 위치
-					sr = i;
-					sc = j;
+				// 민식이의 현재 위치
+				if (map[r][c] == '0') {
+					sr = r;
+					sc = c;
+
+					map[r][c] = '.'; // 빈칸으로 변경
 				}
 			}
 		}
-		////////////////////// end of Input
 
-		isVisited = new boolean[N][M][64];
 		System.out.println(bfs(sr, sc));
+	}
+
+	static int bfs(int sr, int sc) {
+		Queue<Coord> dq = new ArrayDeque<>();
+		int[][][] isVisited = new int[N][M][64]; // 방문 체크 배열, 열쇠 6개 => 2^6
+
+		isVisited[sr][sc][0] = 1; // 시작점 방문 체크
+		dq.offer(new Coord(sr, sc, 0)); // 시작점 덱 삽입
+
+		while (!dq.isEmpty()) {
+			Coord cur = dq.poll();
+			int r = cur.r;
+			int c = cur.c;
+			int key = cur.key;
+
+			if (map[r][c] == '1') // 출구에 도착한 경우
+				return isVisited[r][c][key] - 1;
+
+			for (int d = 0; d < 4; d++) { // 4방향 탐색
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+
+				// 유효범위 외 || 이미 방문 || 벽
+				if (!isValidCoord(nr, nc) || isVisited[nr][nc][key] != 0 || map[nr][nc] == '#')
+					continue; // 넘기기
+
+				if (map[nr][nc] == '.' || map[nr][nc] == '1') { // 빈칸과 문인 경우
+					isVisited[nr][nc][key] = isVisited[r][c][key] + 1;
+					dq.offer(new Coord(nr, nc, key));
+				} else if (isKey(nr, nc)) { // 열쇠인 경우
+					int nKey = key | (1 << map[nr][nc] - 'a'); // 열쇠 획득
+
+					isVisited[nr][nc][nKey] = isVisited[r][c][key] + 1;
+					dq.offer(new Coord(nr, nc, nKey));
+				} else if (isDoor(nr, nc)) { // 문인 경우
+					if ((key & (1 << (map[nr][nc] - 'A'))) == 0) // 열쇠가 없는 경우
+						continue;
+
+					isVisited[nr][nc][key] = isVisited[r][c][key] + 1;
+					dq.offer(new Coord(nr, nc, key));
+				}
+			}
+		}
+
+		return -1; // 탈출 못한 경우
+	}
+
+	static boolean isDoor(int r, int c) {
+		return 'A' <= map[r][c] && map[r][c] <= 'Z';
+	}
+
+	static boolean isKey(int r, int c) {
+		return 'a' <= map[r][c] && map[r][c] <= 'z';
 	}
 
 	static boolean isValidCoord(int r, int c) {
 		return -1 < r && r < N && -1 < c && c < M;
 	}
 
-	static int bfs(int sr, int sc) {
-		Queue<int[]> dq = new ArrayDeque<>();
+	static class Coord {
+		int r, c; // 좌표
+		int key; // 지참 열쇠
 
-		// 시작점
-		isVisited[sr][sc][0] = true;
-		dq.offer(new int[] { sr, sc, 0, 0 });
-
-		while (!dq.isEmpty()) {
-			int[] cur = dq.poll();
-			int r = cur[0];
-			int c = cur[1];
-			int key = cur[2];
-			int time = cur[3];
-
-			// 출구에 도착한 경우
-			if (map[r][c] == '1')
-				return time;
-
-			// 4방향 탐색 실행
-			for (int i = 0; i < 4; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-
-				// 유효범위 밖 || 방문 || 벽
-				if (!isValidCoord(nr, nc) || isVisited[nr][nc][key] || map[nr][nc] == '#')
-					continue;
-                
-                // 빈 칸인 경우
-                if(map[nr][nc] == '.' || map[nr][nc] == '0' || map[nr][nc] == '1') {
-					isVisited[nr][nc][key] = true;
-					dq.offer(new int[] { nr, nc, key, time + 1 });
-				}
-                
-				// 열쇠를 발견한 경우
-				else if ('a' <= map[nr][nc] && map[nr][nc] <= 'f') {
-					int nKey = key | (1 << (map[nr][nc] - 'a')); // 새로운 열쇠를 획득
-					isVisited[nr][nc][nKey] = true;
-					dq.offer(new int[] { nr, nc, nKey, time + 1 });
-				}
-
-				// 문을 발견한 경우
-				else if ('A' <= map[nr][nc] && map[nr][nc] <= 'F') {
-					if ((key & (1 << (map[nr][nc] - 'A'))) != 0) { // 열쇠가 있으면
-						isVisited[nr][nc][key] = true;
-						dq.offer(new int[] { nr, nc, key, time + 1 });
-					}
-				}
-			}
+		Coord(int r, int c, int key) {
+			this.r = r;
+			this.c = c;
+			this.key = key;
 		}
-		return -1; // 민식이가 미로를 탈출 할 수 없는 경우
 	}
 }
