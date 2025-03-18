2ddbee04d32990ca4d4596276db6843d276e65b9
yoon <72852508+0ct25th@users.noreply.github.com>
2025-03-18 15:25:33 +0900

    [Gold IV] Title: 플로이드, Time: 324 ms, Memory: 42000 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,80 +1,55 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+	static final int INF = 10000001;
 	static int n, m;
 	static int[][] map;
-	static final int INF = 100000001;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		// 첫째 줄에 도시의 개수 n이 주어지고
-		n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine()); // 도시 개수
+		m = Integer.parseInt(br.readLine()); // 버스 개수
 
-		// 둘째 줄에는 버스의 개수 m이 주어진다.
-		m = Integer.parseInt(br.readLine());
-
-		// 초기화
-		map = new int[n][n];
-		for (int i = 0; i < n; i++)
+		map = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++)
 			Arrays.fill(map[i], INF);
 
-		// 셋째 줄부터 m+2줄까지 다음과 같은 버스의 정보가 주어진다.
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()); // 시작 도시
+			int b = Integer.parseInt(st.nextToken()); // 도착 도시
+			int c = Integer.parseInt(st.nextToken()); // 필요 비용
 
 			// 시작 도시와 도착 도시를 연결하는 노선은 하나가 아닐 수 있다.
-			if (map[a - 1][b - 1] > c)
-				map[a - 1][b - 1] = c;
+			if (map[a][b] > c)
+				map[a][b] = c;
 		}
 
-		///////////////// end of Input
+		floydWarshall();
 
-		// 플로이드워셜 실행
-		floyd();
+		for (int r = 1; r <= n; r++) {
+			for (int c = 1; c <= n; c++) {
+				if (map[r][c] >= INF || r == c)
+					map[r][c] = 0;
 
-		// n개의 줄을 출력해야 한다. i번째 줄에 출력하는 j번째 숫자는 도시 i에서 j로 가는데 필요한 최소 비용이다.
-		// 만약, i에서 j로 갈 수 없는 경우에는 그 자리에 0을 출력한다.
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[i][j] >= INF)
-					map[i][j] = 0;
-				
-				if (i == j)
-					map[i][i] = 0;
-
-				sb.append(map[i][j]).append(" ");
+				sb.append(map[r][c]).append(" ");
 			}
 			sb.append("\n");
 		}
 
-		// 결과 출력
-		bw.write(sb.toString());
-
-		// close
-		br.close();
-		bw.close();
+		System.out.println(sb);
 	}
 
-	static void floyd() {
-		for (int k = 0; k < n; k++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
+	static void floydWarshall() {
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++)
 					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
-				}
 			}
 		}
 	}
