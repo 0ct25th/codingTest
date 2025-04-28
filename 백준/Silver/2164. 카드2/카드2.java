111f7c05e148ea551232352eedd488c156e536c2
yoon <72852508+0ct25th@users.noreply.github.com>
2025-04-28 17:43:30 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 29000 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -3,24 +3,26 @@ import java.util.*;
 
 public class Main {
 
+	static int N;
+
 	public static void main(String[] args) throws IOException {
-		Scanner scan = new Scanner(System.in);
-		
-		int N = Integer.parseInt(scan.next());
-		Queue<Integer> dq = new ArrayDeque<>();
-		
-		// 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
-		for(int i = 1; i <= N; i++) 
-			dq.offer(i);
-		
-		
-		while(dq.size() != 1) {
-			dq.poll();
-			dq.offer(dq.poll());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++)
+			dq.offerLast(i);
+
+		while (dq.size() > 1) {
+			dq.pollFirst();
+
+			if (dq.size() == 1)
+				break;
+
+			dq.offerLast(dq.pollFirst());
 		}
-		
+
 		System.out.println(dq.poll());
-		
-		scan.close();
 	}
 }
