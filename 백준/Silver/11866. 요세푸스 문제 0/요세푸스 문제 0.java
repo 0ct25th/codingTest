58372ebf91023bf79ea4b288334b964df3eb2893
yoon <72852508+0ct25th@users.noreply.github.com>
2025-04-28 17:26:08 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -2,45 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+
 	static int N, K;
-	static Queue<Integer> dq;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
-		// 첫째 줄에 N과 K가 빈 칸을 사이에 두고 순서대로 주어진다. (1 ≤ K ≤ N ≤ 1,000)
+
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		////////////////////////////////////// input
-		
-		dq = new ArrayDeque<>();
-		for(int i = 1; i <= N; i++) 
-			dq.offer(i);
-		
+
+		LinkedList<Integer> list = new LinkedList<>();
+		for (int i = 1; i <= N; i++)
+			list.add(i);
 
 		sb.append("<");
-		int index = 1;
-		while(dq.size() != 1) {
-			if (index == K) {
-				sb.append(dq.poll()).append(", ");
-				index = 1;
-			} else {
-				dq.offer(dq.poll());
-				index++;
-			}
+		int idx = K - 1;
+		while (list.size() > 1) {
+			idx = idx % list.size();
+			sb.append(list.remove(idx)).append(", ");
+			idx += K - 1;
 		}
-		
-		sb.append(dq.poll()).append(">");
-		
+		sb.append(list.get(0)).append(">");
+
 		bw.write(sb.toString());
 		bw.flush();
-		
+
 		br.close();
 		bw.close();
 	}
