22f969de253774fca00265953b3f35df214dae85
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-01 11:59:39 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Main {
 
 	static int N, M;
-	static boolean[][] map, isVisited;
+	static int[][] map;
+	static boolean[][] isVisited;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
@@ -15,54 +16,46 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		map = new boolean[N][M];
+		map = new int[N][M];
 		for (int r = 0; r < N; r++) {
 			String str = br.readLine();
-			for (int c = 0; c < M; c++) {
-				if (str.charAt(c) == '1')
-					map[r][c] = true; // 이동할 수 있는 칸
-				else
-					map[r][c] = false; // 이동할 수 없는 칸
-			}
+			for (int c = 0; c < M; c++)
+				map[r][c] = str.charAt(c) - '0';
 		}
-		////////////////////// end of input
 
-		System.out.println(bfs(0, 0) + 1);
+		isVisited = new boolean[N][M];
+
+		System.out.println(bfs(0, 0));
 	}
 
 	static int bfs(int sr, int sc) {
 		Queue<Coord> dq = new ArrayDeque<>();
-		isVisited = new boolean[N][M];
 
-		// 시작점: 0, 0
-		dq.offer(new Coord(sr, sc, 0));
 		isVisited[sr][sc] = true;
+		dq.offer(new Coord(sr, sc, 1));
 
 		while (!dq.isEmpty()) {
 			Coord cur = dq.poll();
 			int r = cur.r;
 			int c = cur.c;
-			int cell = cur.cell;
+			int move = cur.move;
 
-			// 도착점(N-1, M-1)에 도착한 경우
 			if (r == N - 1 && c == M - 1)
-				return cell;
+				return move;
 
-			// 4방향 탐색: 서로 인접한 칸으로만 이동 가능
-			for(int d = 0; d < 4; d++) {
+			for (int d = 0; d < 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
-				
-				// 유효범위를 벗어난 경우 || 이미 방문한 경우 || 이동할 수 없는 칸인 경우
-				if(!isValidCoord(nr, nc) || isVisited[nr][nc] || !map[nr][nc])
-					continue; // 넘김
-				
-				isVisited[nr][nc] = true; // 방문 체크
-				dq.offer(new Coord(nr, nc, cell + 1)); // 덱 삽입
+
+				if (!isValidCoord(nr, nc) || isVisited[nr][nc] || map[nr][nc] == 0)
+					continue;
+
+				isVisited[nr][nc] = true;
+				dq.offer(new Coord(nr, nc, move + 1));
 			}
 		}
 
-		return -1; // 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+		return -1;
 	}
 
 	static boolean isValidCoord(int r, int c) {
@@ -70,14 +63,13 @@ public class Main {
 	}
 
 	static class Coord {
-		int r;
-		int c;
-		int cell;
+		int r, c;
+		int move;
 
-		Coord(int r, int c, int cell) {
+		Coord(int r, int c, int move) {
 			this.r = r;
 			this.c = c;
-			this.cell = cell;
+			this.move = move;
 		}
 	}
 }
