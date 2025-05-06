20100083403f3a088d71335d2054de69a8fc75a4
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-07 00:36:30 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 492 ms, Memory: 62324 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -4,79 +4,73 @@ import java.util.*;
 public class Main {
 
 	static int K, W, H;
-	static int[][] map; // 0: 평지, 1: 장애물
-	// 원숭이 움직임
-	static int[] mDr = { -1, 1, 0, 0 };
-	static int[] mDc = { 0, 0, -1, 1 };
-	// 말 움직임
-	static int[] hDr = { -2, -2, -1, -1, 1, 1, 2, 2 };
-	static int[] hDc = { -1, 1, -2, 2, -2, 2, -1, 1 };
+	static int[][] map;
+	static int[] drM = { -1, 1, 0, 0 };
+	static int[] dcM = { 0, 0, -1, 1 };
+	static int[] drH = { -2, -2, -1, -1, 1, 1, 2, 2 };
+	static int[] dcH = { -1, 1, -2, 2, -2, 2, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		// K번만 말의 움직임으로 움직일 수 있음
 		K = Integer.parseInt(br.readLine());
 
 		st = new StringTokenizer(br.readLine());
-		W = Integer.parseInt(st.nextToken()); // 가로 길이
-		H = Integer.parseInt(st.nextToken()); // 세로 길이
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
 
 		map = new int[H][W];
 		for (int r = 0; r < H; r++) {
 			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < W; c++) {
+			for (int c = 0; c < W; c++)
 				map[r][c] = Integer.parseInt(st.nextToken());
-			}
 		}
 
 		System.out.println(bfs(0, 0));
 	}
 
 	static int bfs(int sr, int sc) {
-		Queue<Monkey> dq = new ArrayDeque<>();
-		int[][][] isVisited = new int[H][W][K + 1];
+		Queue<Coord> dq = new ArrayDeque<>();
+		boolean[][][] isVisited = new boolean[K + 1][H][W];
 
-		isVisited[sr][sc][0] = 0;
-		dq.offer(new Monkey(sr, sc, 0));
+		isVisited[0][sr][sc] = true;
+		dq.offer(new Coord(sr, sc, 0, 0));
 
 		while (!dq.isEmpty()) {
-			Monkey cur = dq.poll();
+			Coord cur = dq.poll();
 			int r = cur.r;
 			int c = cur.c;
-			int cnt = cur.count;
+			int k = cur.k;
+			int m = cur.m;
 
 			if (r == H - 1 && c == W - 1)
-				return isVisited[r][c][cnt];
+				return m;
 
-			// 말로 변할 수 있는 경우
-			if (cnt < K) {
-				// 말로 8방향 탐색
+			// 말
+			if (k + 1 <= K) {
 				for (int d = 0; d < 8; d++) {
-					int nr = r + hDr[d];
-					int nc = c + hDc[d];
+					int nr = r + drH[d];
+					int nc = c + dcH[d];
 
-					// 유효범위 외 || 이미 방문 || 장애물
-					if (!isValidCoord(nr, nc) || isVisited[nr][nc][cnt + 1] != 0 || map[nr][nc] == 1)
-						continue; // 넘기기
+					if (!isValidCoord(nr, nc) || map[nr][nc] == 1 || isVisited[k + 1][nr][nc])
+						continue;
 
-					isVisited[nr][nc][cnt + 1] = isVisited[r][c][cnt] + 1;
-					dq.offer(new Monkey(nr, nc, cnt + 1));
+					isVisited[k + 1][nr][nc] = true;
+					dq.offer(new Coord(nr, nc, k + 1, m + 1));
 				}
 			}
 
-			// 원숭이로 4방향 탐색
+			// 원숭이
 			for (int d = 0; d < 4; d++) {
-				int nr = r + mDr[d];
-				int nc = c + mDc[d];
+				int nr = r + drM[d];
+				int nc = c + dcM[d];
 
-				// 유효범위 외 || 이미 방문 || 장애물
-				if (!isValidCoord(nr, nc) || isVisited[nr][nc][cnt] != 0 || map[nr][nc] == 1)
-					continue; // 넘기기
+				if (!isValidCoord(nr, nc) || map[nr][nc] == 1 || isVisited[k][nr][nc])
+					continue;
 
-				isVisited[nr][nc][cnt] = isVisited[r][c][cnt] + 1;
-				dq.offer(new Monkey(nr, nc, cnt));
+				isVisited[k][nr][nc] = true;
+				dq.offer(new Coord(nr, nc, k, m + 1));
 			}
 		}
 
@@ -87,15 +81,16 @@ public class Main {
 		return -1 < r && r < H && -1 < c && c < W;
 	}
 
-	static class Monkey {
-		int r;
-		int c;
-		int count; // 말로 변한 횟수
+	static class Coord {
+		int r, c;
+		int k; // 능력 사용
+		int m; // 동작수
 
-		Monkey(int r, int c, int count) {
+		Coord(int r, int c, int k, int m) {
 			this.r = r;
 			this.c = c;
-			this.count = count;
+			this.k = k;
+			this.m = m;
 		}
 	}
 }
