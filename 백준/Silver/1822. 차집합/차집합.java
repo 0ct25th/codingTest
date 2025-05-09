c3413469a5d5db67b0528aaaccb1f0b15e48bbca
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-09 19:08:45 +0900

    [Silver IV] Title: 차집합, Time: 1604 ms, Memory: 158028 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -3,8 +3,9 @@ import java.util.*;
 
 public class Main {
 
-	static int A, B;
-	static Set<Integer> set;
+	static int a, b;
+	static int[] A, B;
+	static List<Integer> lst;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,23 +13,55 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 
 		st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
 
-		set = new TreeSet<>();
-
+		A = new int[a];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < A; i++)
-			set.add(Integer.parseInt(st.nextToken()));
+		for (int i = 0; i < a; i++)
+			A[i] = Integer.parseInt(st.nextToken());
 
+		B = new int[b];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < B; i++)
-			set.remove(Integer.parseInt(st.nextToken()));
+		for (int i = 0; i < b; i++)
+			B[i] = Integer.parseInt(st.nextToken());
 
-		sb.append(set.size()).append("\n");
-		for (int i : set)
-			sb.append(i).append(" ");
+		Arrays.sort(B);
+		lst = new ArrayList<>();
 
+		for (int i = 0; i < a; i++) {
+			if (binarySearch(A[i]))
+				lst.add(A[i]);
+		}
+		
+		Collections.sort(lst);
+
+		if (lst.size() > 0) {
+			sb.append(lst.size()).append("\n");
+
+			for (int i : lst)
+				sb.append(i).append(" ");
+		} else
+			sb.append(0);
+		
 		System.out.println(sb);
+	}
+
+	static boolean binarySearch(int x) {
+		int start = 0;
+		int end = b - 1;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			if (B[mid] == x)
+				return false;
+			else if (B[mid] < x)
+				start = mid + 1;
+			else
+				end = mid - 1;
+		}
+
+		return true;
 	}
 }
