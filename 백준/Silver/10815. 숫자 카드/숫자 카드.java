a461dd517254bf639eadb371cbac85ccc503991b
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-09 18:21:01 +0900

    [Silver V] Title: 숫자 카드, Time: 844 ms, Memory: 143192 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,50 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	static int N, M;
-	static HashMap<Integer, Integer> cards;
+	static Map<Integer, Integer> map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		// 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다.
 		N = Integer.parseInt(br.readLine());
 
-		// 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다.
+		map = new HashMap<>();
 		st = new StringTokenizer(br.readLine());
-		cards = new HashMap<Integer, Integer>();
 		for (int i = 0; i < N; i++) {
-			cards.put(Integer.parseInt(st.nextToken()), 1);
+			int num = Integer.parseInt(st.nextToken());
+
+			if (map.containsKey(num))
+				continue;
+
+			map.put(num, 1);
 		}
 
-		// 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다.
 		M = Integer.parseInt(br.readLine());
 
-		// 넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다.
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
-			if (cards.containsKey(Integer.parseInt(st.nextToken())))
-				sb.append(1).append(" ");
+			int num = Integer.parseInt(st.nextToken());
+
+			if (map.containsKey(num))
+				sb.append("1 ");
 			else
-				sb.append(0).append(" ");
+				sb.append("0 ");
 		}
 
-		// 결과 출력
-		bw.write(sb.toString());
-		bw.flush();
-
-		// close
-		br.close();
-		bw.close();
+		System.out.println(sb);
 	}
 }
