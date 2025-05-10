20bf9cb6f8dc8bc9135dd116e7e0a4ffac9ece78
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-10 19:34:40 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1140 ms, Memory: 126492 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
 
-	static int M, N;
-	static long start, mid, end, result;
-	static int[] snacks;
+	static int N, M, result;
+	static int[] L;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,39 +14,37 @@ public class Main {
 		M = Integer.parseInt(st.nextToken()); // 조카의 수
 		N = Integer.parseInt(st.nextToken()); // 과자의 수
 
+		L = new int[N];
 		st = new StringTokenizer(br.readLine());
-		snacks = new int[N];
 		for (int i = 0; i < N; i++)
-			snacks[i] = Integer.parseInt(st.nextToken()); // 과자 길이
+			L[i] = Integer.parseInt(st.nextToken());
 
-		Arrays.sort(snacks);
-
-		System.out.println(binarySearch());
+		Arrays.sort(L);
+		share();
+		System.out.println(result);
 	}
 
-	static long binarySearch() {
-		start = 1;
-		end = snacks[N - 1];
+	static void share() {
+		int start = 1;
+		int end = L[N - 1];
 
 		while (start <= end) {
-			int count = 0; // 나눠줄 수 있는 과자 수
-			mid = (start + end) / 2;
+			int mid = (start + end) / 2;
 
-			// 줄 수 있는 조카 수 계산
-			for (int i = 0; i < N; i++)
-				count += snacks[i] / mid;
-
-			// 과자 수가 조카 수보다 많거나 같은 경우
-			if (count >= M) {
+			if (cut(mid) >= M) {
+				result = mid;
 				start = mid + 1;
-
-				if (result < mid)
-					result = mid; // 막대 과자의 최대 길이 조정
 			} else
 				end = mid - 1;
 		}
-
-		return result;
 	}
 
+	static int cut(int x) {
+		int cnt = 0;
+
+		for (int i = 0; i < N; i++)
+			cnt += (L[i] / x);
+
+		return cnt;
+	}
 }
