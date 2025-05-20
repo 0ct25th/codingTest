17cd4f4f9dede46982134c88e87166e99b807551
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-20 11:20:28 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 140 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -3,39 +3,37 @@ import java.util.*;
 
 public class Main {
 
-	static int N, result;
-	static int[] A, dp;
+	static int N, A[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
+		A = new int[N];
 
 		st = new StringTokenizer(br.readLine());
-		A = new int[N];
 		for (int i = 0; i < N; i++)
 			A[i] = Integer.parseInt(st.nextToken());
-		//////////////////////////// end of Input
 
-		LIS();
-
-		System.out.println(result);
+		System.out.println(LIS());
 	}
 
-	static void LIS() {
-		dp = new int[N];
+	static int LIS() {
+		int result = 0;
+		int[] dp = new int[N];
 
 		for (int i = 0; i < N; i++) {
 			dp[i] = 1;
 
-			// 주어진 수열에 첫번째 부터 i번째 전까지 탐색
-			for (int j = 0; j < i; j++)
-				// 자신보다 큰 값 && 자신을 끝에 놓았을 때 최대값 갱신 여부 확인
-				if (A[i] < A[j] && dp[i] < dp[j] + 1)
-					dp[i] = dp[j] + 1;
+			for (int j = 0; j < i; j++) {
+				if (A[j] > A[i])
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+			}
 
 			result = Math.max(result, dp[i]);
 		}
+
+		return result;
 	}
 }
