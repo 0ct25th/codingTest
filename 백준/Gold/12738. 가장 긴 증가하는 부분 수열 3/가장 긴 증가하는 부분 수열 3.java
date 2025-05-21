0faffd405f9a5dc82c50e5d8c33c787be586876b
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-21 17:08:54 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 624 ms, Memory: 144924 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -4,45 +4,49 @@ import java.util.*;
 public class Main {
 
 	static int N;
-	static int[] A;
-	static List<Integer> LIS;
+	static long A[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
+		A = new long[N];
 
 		st = new StringTokenizer(br.readLine());
-		A = new int[N];
 		for (int i = 0; i < N; i++)
-			A[i] = Integer.parseInt(st.nextToken());
+			A[i] = Long.parseLong(st.nextToken());
 
-		LIS = new ArrayList<>();
-		LIS.add(A[0]);
+		System.out.println(LIS());
+	}
+
+	static int LIS() {
+		List<Long> lis = new ArrayList<>();
+		lis.add(A[0]);
 
 		for (int i = 1; i < N; i++) {
-			int key = A[i];
+			long key = A[i];
 
-			if (LIS.get(LIS.size() - 1) < key)
-				LIS.add(key);
+			if (lis.get(lis.size() - 1) < key)
+				lis.add(key);
 			else {
 				int start = 0;
-				int end = LIS.size() - 1;
+				int end = lis.size() - 1;
 
 				while (start < end) {
 					int mid = (start + end) / 2;
 
-					if (LIS.get(mid) < key)
+					if (lis.get(mid) < key)
 						start = mid + 1;
 					else
 						end = mid;
 				}
 
-				LIS.set(end, key);
+				lis.set(end, key);
 			}
 		}
 
-		System.out.println(LIS.size());
+		return lis.size();
 	}
+
 }
