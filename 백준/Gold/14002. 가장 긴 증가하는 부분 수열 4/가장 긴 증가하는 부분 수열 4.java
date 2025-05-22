a2f5c9326768718563a6633f68e9dfc6bfbf1fae
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-22 10:56:53 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 116 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -3,65 +3,68 @@ import java.util.*;
 
 public class Main {
 
-	static int N, result;
-	static int[] A, dp;
-	static Stack<Integer> stk;
+	static int N, A[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
-
 		A = new int[N];
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++)
 			A[i] = Integer.parseInt(st.nextToken());
-		/////////////////////// end of Input
 
-		// 가장 긴 증가하는 부분 수열의 길이 구하기
 		LIS();
-
-		// 수열 찾기
-		sequence();
-
-		// 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
-		System.out.println(result);
-
-		// 둘째 줄에는 가장 긴 증가하는 부분 수열을 출력한다. 그러한 수열이 여러가지인 경우 아무거나 출력한다.
-		while(!stk.isEmpty())
-			System.out.print(stk.pop() + " ");
-
-		br.close();
-	}
-
-	static void sequence() {
-		int value = result; // 가장 긴 증가하는 부분 수열 길이 값
-		stk = new Stack<>(); // 경로를 역추적할 스택
-
-		for (int i = N - 1; i > -1; i--) {
-			// 현재 찾는 길이와 같은 겨우
-			if (value == dp[i]) {
-				stk.push(A[i]);
-				value--; // 다음 찾을 길이
-			}
-		}
 	}
 
 	static void LIS() {
-		dp = new int[N];
-		result = 1;
+		StringBuilder sb = new StringBuilder();
+		List<Integer> lis = new ArrayList<>();
+		int[] order = new int[N];
 
-		for (int i = 0; i < N; i++) {
-			dp[i] = 1;
+		lis.add(A[0]);
+		order[0] = 0;
+		for (int i = 1; i < N; i++) {
+			int key = A[i];
+			order[i] = 1;
 
-			for (int j = 0; j < i; j++) {
-				if (A[j] < A[i] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
+			if (lis.get(lis.size() - 1) < key) {
+				lis.add(key);
+				order[i] = lis.size() - 1;
+			} else {
+				int start = 0;
+				int end = lis.size() - 1;
+
+				while (start < end) {
+					int mid = (start + end) / 2;
+
+					if (lis.get(mid) < key)
+						start = mid + 1;
+					else
+						end = mid;
 				}
-			}
 
-			result = Math.max(result, dp[i]);
+				lis.set(end, key);
+				order[i] = end;
+			}
 		}
+
+		int idx = lis.size() - 1;
+		Stack<Integer> stk = new Stack<>();
+		for (int i = N - 1; i >= 0; i--) {
+			if (order[i] != idx)
+				continue;
+
+			stk.push(A[i]);
+			idx--;
+		}
+
+		sb.append(lis.size()).append("\n");
+		while (!stk.isEmpty())
+			sb.append(stk.pop()).append(" ");
+
+		System.out.println(sb);
 	}
 }
