b3d0007d0c87166579702f41e4a08879bb25bc07
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-27 12:49:38 +0900

    [Gold IV] Title: 여행 가자, Time: 152 ms, Memory: 16668 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
 	static int N, M;
-	static int[] p, schedule;
+	static int[] p, city;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,33 +13,30 @@ public class Main {
 		N = Integer.parseInt(br.readLine()); // 도시의 수
 		M = Integer.parseInt(br.readLine()); // 여행 계획에 속한 도시들의 수
 
-		// 다음 N개의 줄에는 N개의 정수가 주어진다. i번째 줄의 j번째 수는 i번 도시와 j번 도시의 연결 정보를 의미한다.
-		make();
-		for (int i = 1; i <= N; i++) {
+		init();
+		for (int r = 1; r <= N; r++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				int c = Integer.parseInt(st.nextToken());
+			for (int c = 1; c <= N; c++) {
+				int con = Integer.parseInt(st.nextToken());
 
-				// 연결되지 않은 경우
-				if (c == 0)
+				if (con == 0)
 					continue;
 
-				union(i, j);
+				union(r, c);
 			}
 		}
 
-		// 마지막 줄에는 여행 계획이 주어진다. 도시의 번호는 1부터 N까지 차례대로 매겨져 있다.
-		schedule = new int[M];
+		city = new int[M];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++)
-			schedule[i] = Integer.parseInt(st.nextToken());
+			city[i] = Integer.parseInt(st.nextToken());
 
 		System.out.println(possible());
 	}
 
 	static String possible() {
 		for (int i = 0; i < M - 1; i++) {
-			if (!isUnion(schedule[i], schedule[i + 1]))
+			if (!isUnion(city[i], city[i + 1]))
 				return "NO";
 		}
 
@@ -68,12 +65,12 @@ public class Main {
 
 	static int find(int x) {
 		if (x == p[x])
-			return x;
+			return p[x];
 
 		return p[x] = find(p[x]);
 	}
 
-	static void make() {
+	static void init() {
 		p = new int[N + 1];
 
 		for (int i = 1; i <= N; i++)
