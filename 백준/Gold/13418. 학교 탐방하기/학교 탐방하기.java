8e7a1315c65e11a553b397c32d6072c94bfe6eee
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-29 17:01:42 +0900

    [Gold III] Title: 학교 탐방하기, Time: 748 ms, Memory: 160596 KB -BaekjoonHub

--- 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.java
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.java
@@ -3,64 +3,65 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M, worst, best;
+	static int N, M;
+	static List<Edge> edgeList, ascList;
 	static int[] p;
-	static List<Edge> edgeList;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		// 입력의 첫 번째 줄에는 건물의 개수 N(1 ≤ N ≤ 1,000)과 도로의 개수 M(1 ≤ M ≤ N(N-1)/2) 이 주어진다.
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 정점 수
-		M = Integer.parseInt(st.nextToken()); // 간선 수
+		N = Integer.parseInt(st.nextToken()); // 건물의 개수
+		M = Integer.parseInt(st.nextToken()); // 도로의 개수
 
-		// 입력의 두 번째 줄부터 M+1개의 줄에는 A, B(1 ≤ A, B ≤ N), C 가 주어진다.
-		// 이는 A와 B 건물에 연결된 도로가 있다는 뜻이며, C는 0(오르막길) 또는 1(내리막길)의 값을 가진다.
 		edgeList = new ArrayList<>();
-
 		for (int i = 0; i <= M; i++) {
 			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			int C = Integer.parseInt(st.nextToken());
-
-			edgeList.add(new Edge(A, B, C));
+			edgeList.add(new Edge(a, b, c));
 		}
 
-		// 오르막순 정렬
-		Collections.sort(edgeList, (o1, o2) -> (o1.sort - o2.sort));
-		worst = kruskal();
-
-		// 내리막 순 정렬
-		Collections.sort(edgeList, (o1, o2) -> -(o1.sort - o2.sort));
-		best = kruskal();
-
-		System.out.println(worst - best);
-	}
-
-	static int kruskal() {
-		int asc = 0; // 오르막 수
-
-		// 전처리
-		make();
-
-		// union-find
+		// best 계산
+		init();
+		Collections.sort(edgeList, (o1, o2) -> -(o1.c - o2.c)); // 내리막길순 정렬
+		ascList = new ArrayList<>();
 		int cnt = 0;
+
 		for (Edge e : edgeList) {
-			if (union(e.from, e.to))
+			if (!union(e.a, e.b))
 				continue;
 
-			if (e.sort == 0)
-				asc++;
+			if (e.c == 0) // 오르막길인 경우
+				ascList.add(e);
 
-			if (++cnt == N + 1)
+			if (++cnt == N)
 				break;
 		}
+		int best = (int) Math.pow(ascList.size(), 2);
 
-		return asc * asc;
+		// worst 계산
+		init();
+		Collections.sort(edgeList, (o1, o2) -> (o1.c - o2.c)); // 오르막순 정렬
+		ascList = new ArrayList<>();
+		cnt = 0;
+
+		for (Edge e : edgeList) {
+			if (!union(e.a, e.b))
+				continue;
+			
+			if (e.c == 0) // 오르막길인 경우
+				ascList.add(new Edge(e.a, e.b, e.c));
+
+			if (++cnt == N)
+				break;
+		}
+		int worst = (int) Math.pow(ascList.size(), 2);
+
+		System.out.println(worst - best);
 	}
 
 	static boolean union(int a, int b) {
@@ -68,20 +69,20 @@ public class Main {
 		int bRoot = find(b);
 
 		if (aRoot == bRoot)
-			return true;
+			return false;
 
 		p[bRoot] = aRoot;
-		return false;
+		return true;
 	}
 
 	static int find(int x) {
 		if (x == p[x])
-			return x;
+			return p[x];
 
 		return p[x] = find(p[x]);
 	}
 
-	static void make() {
+	static void init() {
 		p = new int[N + 1];
 
 		for (int i = 0; i <= N; i++)
@@ -89,13 +90,13 @@ public class Main {
 	}
 
 	static class Edge {
-		int from, to;
-		int sort;
+		int a, b; // 번호
+		int c; // 경사도
 
-		Edge(int from, int to, int sort) {
-			this.from = from;
-			this.to = to;
-			this.sort = sort;
+		Edge(int a, int b, int c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
 		}
 	}
 }
