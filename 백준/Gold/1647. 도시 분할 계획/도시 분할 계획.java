6cb26c7c3b3377944d6189680c5f676713d15ef6
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-29 16:36:17 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1532 ms, Memory: 331380 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,99 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int N; // 집의 개수, 정점
-	static int M; // 길의 개수, 간선
-	static ArrayList<Edge> edgeList; // 간선 리스트
-	static int[] p; // 대표자 저장 배열
-
-	static class Edge implements Comparable<Edge> {
-		int from;
-		int to;
-		int weight;
-
-		public Edge(int from, int to, int weight) {
-			super();
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-	}
+	static int N, M;
+	static List<Edge> edgeList;
+	static Queue<Edge> connectEdgeList;
+	static int[] p;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
-		// 첫째 줄에 집의 개수 N, 길의 개수 M이 주어진다.
-		// N은 2이상 100,000이하인 정수이고, M은 1이상 1,000,000이하인 정수이다.
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 집의 개수
+		M = Integer.parseInt(st.nextToken()); // 길의 개수
 
-		// 그 다음 줄부터 M줄에 걸쳐 길의 정보가 A B C 세 개의 정수로 주어지는데
-		// A번 집과 B번 집을 연결하는 길의 유지비가 C (1 ≤ C ≤ 1,000)라는 뜻이다.
 		edgeList = new ArrayList<>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			int C = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-			edgeList.add(new Edge(A, B, C));
+			edgeList.add(new Edge(a, b, c));
 		}
 
-		// 간선 리스트 오름차순 정렬
-		Collections.sort(edgeList);
+		Collections.sort(edgeList, (o1, o2) -> (o1.w - o2.w));
+		init();
 
-		p = new int[N + 1];
-		make();
-
-		// 크루스컬 알고리즘
+		long result = 0;
 		int cnt = 0;
-		long weight = 0;
-		if (N != 2) {
-			for (Edge e : edgeList) {
-				if (!union(e.from, e.to))
-					continue;
+		connectEdgeList = new PriorityQueue<>((o1, o2) -> -(o1.w - o2.w));
+		for (Edge e : edgeList) {
+			if (!union(e.a, e.b))
+				continue;
 
-				weight += e.weight;
-				if (++cnt == N - 2)
-					break;
-			}
+			connectEdgeList.offer(e);
+			result += e.w;
+			if (++cnt == N)
+				break;
 		}
 
-		System.out.println(weight);
+		result -= connectEdgeList.poll().w;
+
+		System.out.println(result);
 	}
 
-	static void make() {
-		for (int i = 1; i < N + 1; i++)
-			p[i] = i;
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+
+		if (aRoot == bRoot)
+			return false;
+
+		p[bRoot] = aRoot;
+		return true;
 	}
 
 	static int find(int x) {
 		if (x == p[x])
-			return x;
+			return p[x];
 
 		return p[x] = find(p[x]);
 	}
 
-	static boolean union(int x, int y) {
-		x = find(x);
-		y = find(y);
+	static void init() {
+		p = new int[N + 1];
 
-		if (x == y)
-			return false;
+		for (int i = 1; i <= N; i++)
+			p[i] = i;
+	}
 
-		p[y] = x;
-		return true;
+	static class Edge {
+		int a, b; // 집 번호
+		int w; // 유지비
+
+		Edge(int a, int b, int w) {
+			this.a = a;
+			this.b = b;
+			this.w = w;
+		}
 	}
 }
