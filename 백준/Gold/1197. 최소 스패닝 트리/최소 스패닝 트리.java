3e14a448f8babdacd5b6d664f8c1b1546823622a
yoon <72852508+0ct25th@users.noreply.github.com>
2025-05-29 12:53:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 528 ms, Memory: 50752 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,93 +3,78 @@ import java.util.*;
 
 public class Main {
 
-	static final int INF = 987654321;
-	static int V, E, result;
-	static List<Vertex>[] adjList;
-	static int[] minDist;
-	static boolean[] isVisited;
+	static int V, E;
+	static List<Edge> edgeList;
+	static int[] p;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
 		V = Integer.parseInt(st.nextToken()); // 정점의 개수
 		E = Integer.parseInt(st.nextToken()); // 간선의 개수
 
-		// 인접 리스트 선언
-		adjList = new ArrayList[V + 1];
-		for (int i = 1; i <= V; i++)
-			adjList[i] = new ArrayList<>();
-
+		edgeList = new ArrayList<>();
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-			int A = Integer.parseInt(st.nextToken()); // 정점
-			int B = Integer.parseInt(st.nextToken()); // 정점
-			int C = Integer.parseInt(st.nextToken()); // 가중치
-
-			// 무방향 그래프
-			adjList[A].add(new Vertex(B, C));
-			adjList[B].add(new Vertex(A, C));
+			edgeList.add(new Edge(a, b, c));
 		}
-		////////////////////// end of Input
 
-		prim(1);
+		Collections.sort(edgeList);
+		init();
+
+		int result = 0;
+		for (Edge e : edgeList) {
+			if (union(e.a, e.b))
+				result += e.w;
+		}
 
 		System.out.println(result);
-
 	}
 
-	static void prim(int start) {
-		Queue<Vertex> pq = new PriorityQueue<>();
-		minDist = new int[V + 1]; // 간선 비용 최소값 저장 배열
-		Arrays.fill(minDist, INF);
-		isVisited = new boolean[V + 1];
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
 
-		// 시작점
-		minDist[start] = 0;
-		pq.offer(new Vertex(1, 0));
+		if (aRoot == bRoot)
+			return false;
 
-		int cnt = 0; // 방문한 정점 개수
-		while (!pq.isEmpty()) {
-			// pq에서 최소 비용 정점 꺼내기
-			Vertex cur = pq.poll();
-
-			// 이미 방문한 정점인 경우
-			if (isVisited[cur.num])
-				continue;
-
-			isVisited[cur.num] = true; // 방문 처리
-			result += minDist[cur.num]; // 비용 누적
-
-			// 모든 정점 방문한 경우
-			if (++cnt == V)
-				return;
-
-			for (Vertex next : adjList[cur.num]) {
-				// 이미 방문한 경우 || 최소 비용이 더 작은 경우
-				if (isVisited[next.num] || minDist[next.num] <= next.weight)
-					continue;
-
-				minDist[next.num] = next.weight;
-				pq.offer(new Vertex(next.num, minDist[next.num]));
-			}
-		}
-
+		p[bRoot] = aRoot;
+		return true;
 	}
 
-	static class Vertex implements Comparable<Vertex> {
-		int num, weight;
+	static int find(int x) {
+		if (x == p[x])
+			return p[x];
 
-		Vertex(int num, int weight) {
-			this.num = num;
-			this.weight = weight;
+		return p[x] = find(p[x]);
+	}
+
+	static void init() {
+		p = new int[V + 1];
+
+		for (int i = 1; i <= V; i++)
+			p[i] = i;
+	}
+
+	static class Edge implements Comparable<Edge> {
+		int a, b; // 노드 번호
+		int w; // 가중치
+
+		Edge(int a, int b, int w) {
+			this.a = a;
+			this.b = b;
+			this.w = w;
 		}
 
 		@Override
-		public int compareTo(Vertex o) {
-			return Integer.compare(this.weight, o.weight);
+		public int compareTo(Edge o) {
+			return Integer.compare(this.w, o.w);
 		}
 	}
+
 }
