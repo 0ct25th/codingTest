e2392078c3a3fb198da019219ca3dee560365cd0
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-05 14:08:36 +0900

    [Gold III] Title: 다리 만들기, Time: 312 ms, Memory: 142740 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -3,135 +3,145 @@ import java.util.*;
 
 public class Main {
 
-    static int N, map[][], result;
-    static List<Coord> land;
-    static boolean[][] isVisited;
-    static int[] dr = { -1, 1, 0, 0 };
-    static int[] dc = { 0, 0, -1, 1 };
+	static int N, map[][];
+	static List<Coord> list;
+	static Map<Integer, Integer> hash;
+	static boolean[][] isVisited;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
-        isVisited = new boolean[N][N]; // N 초기화 후 배열 생성
-        land = new ArrayList<>();
-        for (int r = 0; r < N; r++) {
-            st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < N; c++) {
-                map[r][c] = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		list = new ArrayList<>();
+		for (int r = 0; r < N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 0; c < N; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
 
-                if (map[r][c] == 1)
-                    land.add(new Coord(r, c));
-            }
-        }
-        ///////////// end of Input
+				if (map[r][c] == 1)
+					list.add(new Coord(r, c));
+			}
+		}
 
-        int idx = 2;
-        for (Coord coord : land) {
-            if (map[coord.r][coord.c] == 1) {
-                bfs(coord.r, coord.c, idx);
-                idx++;
-            }
-        }
+		// 섬 번호 붙이기
+		hash = new HashMap<>();
+		isVisited = new boolean[N][N];
+		int idx = 1;
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				if (isVisited[r][c] || map[r][c] == 0)
+					continue;
 
-        result = Integer.MAX_VALUE;
-        
-        // 모든 육지 좌표에서 다리 건설 시도
-        for (Coord coord : land) {
-            result = Math.min(result, calc(coord.r, coord.c));
-        }
+				bfs(r, c, idx);
+				idx++;
+			}
+		}
 
-        System.out.println(result);
-    }
+		int result = Integer.MAX_VALUE;
+		for (Coord coord : list)
+			result = Math.min(result, bfs(coord.r, coord.c));
 
-    static int calc(int sr, int sc) {
-        Queue<Coord> dq = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][N]; // 각 시작점마다 새로운 방문 배열
+		System.out.println(result);
+	}
 
-        visited[sr][sc] = true;
-        dq.offer(new Coord(sr, sc, 0));
-        int startLand = map[sr][sc]; // 시작 대륙 번호 저장
+	// 다른 섬을 찾는 bfs
+	static int bfs(int sr, int sc) {
+		Queue<Coord> dq = new ArrayDeque<>();
+		isVisited = new boolean[N][N];
 
-        while (!dq.isEmpty()) {
-            Coord cur = dq.poll();
-            int r = cur.r;
-            int c = cur.c;
-            int cnt = cur.d;
+		isVisited[sr][sc] = true;
+		dq.offer(new Coord(sr, sc, hash.get(convert(sr, sc)), 0));
 
-            for (int d = 0; d < 4; d++) {
-                int nr = r + dr[d];
-                int nc = c + dc[d];
+		while (!dq.isEmpty()) {
+			Coord cur = dq.poll();
+			int r = cur.r;
+			int c = cur.c;
+			int n = cur.n;
+			int dist = cur.dist;
 
-                // 유효범위 밖 || 이미 방문
-                if (!isValidCoord(nr, nc) || visited[nr][nc])
-                    continue; // 넘기기
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
 
-                // 다른 대륙 찾은 경우
-                if (map[nr][nc] != 0 && map[nr][nc] != startLand)
-                    return cnt;
-                
-                // 바다인 경우 (거리 증가)
-                if (map[nr][nc] == 0) {
-                    visited[nr][nc] = true;
-                    dq.offer(new Coord(nr, nc, cnt + 1));
-                }
-                
-                // 같은 대륙인 경우 (거리 증가 없음)
-                else if (map[nr][nc] == startLand) {
-                    visited[nr][nc] = true;
-                    dq.offer(new Coord(nr, nc, cnt));
-                }
-            }
-        }
+				if (!isValidCoord(nr, nc) || isVisited[nr][nc])
+					continue;
 
-        return Integer.MAX_VALUE;
-    }
+				// 섬인 경우
+				if (map[nr][nc] == 1) {
+					// 같은 섬인 경우
+					if (n == hash.get(convert(nr, nc)))
+						continue;
+					// 다른 섬인 경우
+					else
+						return dist;
+				}
+				// 바다인 경우
+				else {
+					isVisited[nr][nc] = true;
+					dq.offer(new Coord(nr, nc, n, dist + 1));
+				}
+			}
 
-    static void bfs(int sr, int sc, int idx) {
-        Queue<Coord> dq = new ArrayDeque<>();
+		}
 
-        map[sr][sc] = idx;
-        dq.offer(new Coord(sr, sc));
+		return Integer.MAX_VALUE;
+	}
 
-        while (!dq.isEmpty()) {
-            Coord cur = dq.poll();
-            int r = cur.r;
-            int c = cur.c;
+	// 같은 섬을 찾는 bfs
+	static void bfs(int sr, int sc, int idx) {
+		Queue<Coord> dq = new ArrayDeque<>();
 
-            for (int d = 0; d < 4; d++) {
-                int nr = r + dr[d];
-                int nc = c + dc[d];
+		hash.put(convert(sr, sc), idx);
+		isVisited[sr][sc] = true;
+		dq.offer(new Coord(sr, sc));
 
-                // 유효범위 밖 || 방문 완료(다른 번호) || 바다
-                if (!isValidCoord(nr, nc) || map[nr][nc] != 1)
-                    continue; // 넘기기
+		while (!dq.isEmpty()) {
+			Coord cur = dq.poll();
+			int r = cur.r;
+			int c = cur.c;
 
-                map[nr][nc] = idx; // 대륙 번호 변경
-                dq.offer(new Coord(nr, nc));
-            }
-        }
-    }
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
 
-    static boolean isValidCoord(int r, int c) {
-        return -1 < r && r < N && -1 < c && c < N;
-    }
+				if (!isValidCoord(nr, nc) || isVisited[nr][nc] || map[nr][nc] == 0)
+					continue;
 
-    static class Coord{
-        int r, c;
-        int d; // 거리
+				hash.put(convert(nr, nc), idx);
+				isVisited[nr][nc] = true;
+				dq.offer(new Coord(nr, nc));
+			}
+		}
+	}
 
-        Coord(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
+	static boolean isValidCoord(int r, int c) {
+		return -1 < r && r < N && -1 < c && c < N;
+	}
 
-        Coord(int r, int c, int d) {
-            this.r = r;
-            this.c = c;
-            this.d = d;
-        }
-    }
+	static int convert(int r, int c) {
+		return r * 1000 + c;
+	}
+
+	static class Coord {
+		int r, c;
+		int n; // 섬 번호
+		int dist; // 이동 거리
+
+		Coord(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+
+		Coord(int r, int c, int n, int dist) {
+			this.r = r;
+			this.c = c;
+			this.n = n;
+			this.dist = dist;
+		}
+	}
 }
