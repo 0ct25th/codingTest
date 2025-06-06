01c6fe2c5b42268659525b2f4857c0a9b940e1e1
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-06 22:31:12 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,25 +2,24 @@ import java.io.*;
 
 public class Main {
 
-	static int T, n, dp[];
+	static int[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		T = Integer.parseInt(br.readLine());
 
 		dp = new int[12];
 		dp[1] = 1;
 		dp[2] = 2;
 		dp[3] = 4;
 
-		for (int i = 4; i < 12; i++)
-			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		for (int i = 4; i <= 11; i++)
+			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
 
+		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
-			n = Integer.parseInt(br.readLine());
-
+			int n = Integer.parseInt(br.readLine());
+			
 			System.out.println(dp[n]);
-		} // end of TestCase
+		}
 	}
 }
