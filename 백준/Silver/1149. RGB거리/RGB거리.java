40888f456370484ad0a515773d75422905854d5d
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-09 13:02:52 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -8,7 +8,7 @@ public class Main {
 	final static int BLUE = 2;
 
 	static int N;
-	static int[][] cost, dp;
+	static int[][] home, dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,32 +16,25 @@ public class Main {
 
 		N = Integer.parseInt(br.readLine());
 
-		// 입력받은 비용을 저장할 배열
-		cost = new int[N][3];
-		for (int r = 0; r < N; r++) {
+		home = new int[N + 1][3];
+		for (int r = 1; r <= N; r++) {
 			st = new StringTokenizer(br.readLine());
-
-			cost[r][RED] = Integer.parseInt(st.nextToken());
-			cost[r][GREEN] = Integer.parseInt(st.nextToken());
-			cost[r][BLUE] = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < 3; c++) {
+				int w = Integer.parseInt(st.nextToken());
+				home[r][c] = w;
+			}
 		}
 
-		// dp 배열 초기화
-		dp = new int[N][3];
-
-		// 첫 번째 집의 색칠 비용을 dp에 저장
-		dp[0][RED] = cost[0][RED];
-		dp[0][GREEN] = cost[0][GREEN];
-		dp[0][BLUE] = cost[0][BLUE];
-
-		// 1부터 N-1까지 각 i별 i-1의 서로 다른 색상 중 최솟값을 누적하여 더함
-		for (int i = 1; i < N; i++) {
-			dp[i][RED] = cost[i][RED] + Math.min(dp[i - 1][GREEN], dp[i - 1][BLUE]);
-			dp[i][GREEN] = cost[i][GREEN] + Math.min(dp[i - 1][RED], dp[i - 1][BLUE]);
-			dp[i][BLUE] = cost[i][BLUE] + Math.min(dp[i - 1][RED], dp[i - 1][GREEN]);
+		dp = new int[N + 1][3];
+		dp[1][RED] = home[1][RED];
+		dp[1][GREEN] = home[1][GREEN];
+		dp[1][BLUE] = home[1][BLUE];
+		for (int i = 2; i <= N; i++) {
+			dp[i][RED] = Math.min(dp[i - 1][GREEN], dp[i - 1][BLUE]) + home[i][RED];
+			dp[i][GREEN] = Math.min(dp[i - 1][RED], dp[i - 1][BLUE]) + home[i][GREEN];
+			dp[i][BLUE] = Math.min(dp[i - 1][GREEN], dp[i - 1][RED]) + home[i][BLUE];
 		}
 
-		// 최종 결과 출력
-		System.out.println(Math.min(dp[N - 1][RED], Math.min(dp[N - 1][GREEN], dp[N - 1][BLUE])));
+		System.out.println(Math.min(dp[N][RED], Math.min(dp[N][GREEN], dp[N][BLUE])));
 	}
 }
