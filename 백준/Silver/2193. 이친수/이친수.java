9d3cc1fedf1ad6625f7109dfaee9ccdf5cbc0de4
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-12 19:17:42 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -3,20 +3,23 @@ import java.io.*;
 public class Main {
 
 	static int N;
-	static long[] dp = new long[91];
+	static long dp[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
 
-		dp[1] = 1;
-		dp[2] = 1;
-		dp[3] = 2;
+		if (N < 3)
+			System.out.println(1);
+		else {
+			dp = new long[N + 1];
+			dp[1] = 1;
+			dp[2] = 1;
+			for (int i = 3; i <= N; i++)
+				dp[i] = dp[i - 1] + dp[i - 2];
 
-		for (int i = 4; i <= N; i++)
-			dp[i] = dp[i - 1] + dp[i - 2];
-		
-		System.out.println(dp[N]);
+			System.out.println(dp[N]);
+		}
 	}
 }
