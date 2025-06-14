032c9a683bc9f92af05db7f5468277210ca67dcb
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-14 14:35:48 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,23 +4,24 @@ public class Main {
 
 	static int N;
 	static long dp[];
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		dp = new long[101];
 		dp[1] = 1;
 		dp[2] = 1;
 		dp[3] = 1;
-
+		dp[4] = 2;
+		dp[5] = 2;
+		for(int i = 6; i <= 100; i++)
+			dp[i] = dp[i - 1] + dp[i - 5];
+		
 		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
+		for(int t = 0; t < T; t++) {
 			N = Integer.parseInt(br.readLine());
-
-			for (int i = 4; i <= N; i++)
-				dp[i] = dp[i - 3] + dp[i - 2];
-
+			
 			System.out.println(dp[N]);
-		} // end of TestCase
+		}
 	}
 }
