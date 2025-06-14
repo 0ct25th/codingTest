cd931003d8c5095036d042e2e6b841f3f7ea867e
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-14 15:08:40 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,46 +3,33 @@ import java.util.*;
 
 public class Main {
 
-	static int N, T[], P[], result;
+	static int N;
+	static int[] T, P, dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
+		T = new int[21];
+		P = new int[21];
+		dp = new int[21];
 
-		T = new int[N];
-		P = new int[N];
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-
 			T[i] = Integer.parseInt(st.nextToken());
 			P[i] = Integer.parseInt(st.nextToken());
 		}
-		////////////// end of Input
 
-		result = Integer.MIN_VALUE;
-		recursion(0, 0);
+		for (int i = 1; i <= 16; i++) {
+			int day = i + T[i];
+			if (day > 20)
+				continue;
 
-		System.out.println(result);
-	}
-
-	static void recursion(int depth, int sum) {
-		// 기저 조건: 퇴사 일정을 넘는 경우
-		if (depth > N) {
-			return;
+			dp[i] = Math.max(dp[i], dp[i - 1]);
+			dp[day] = Math.max(dp[day], dp[i] + P[i]);
 		}
 
-		// 기저 조건: 모든 일수 고려 완료
-		if (depth == N) {
-			result = Math.max(result, sum);
-			return;
-		}
-
-		// 해당 상담을 선택 한 경우
-		recursion(depth + T[depth], sum + P[depth]);
-
-		// 해당 상담을 선택하지 않은 경우
-		recursion(depth + 1, sum);
+		System.out.println(dp[N + 1]);
 	}
 }
