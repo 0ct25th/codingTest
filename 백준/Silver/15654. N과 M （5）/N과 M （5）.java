507a4ba8e5dba34931608dbc6a054e570a44dd75
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-19 22:37:56 +0900

    [Silver III] Title: N과 M (5), Time: 276 ms, Memory: 28744 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -3,63 +3,53 @@ import java.util.*;
 
 public class Main {
 
-	static StringBuilder sb;
-
 	static int N, M;
-	static int[] numbers, picks;
-	static boolean[] isSelected;
+	static int[] num, order;
+	static boolean[] isVisited;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st;
-		sb = new StringBuilder();
-
-		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
+		num = new int[N];
 		st = new StringTokenizer(br.readLine());
-		numbers = new int[N];
 		for (int i = 0; i < N; i++)
-			numbers[i] = Integer.parseInt(st.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 
-		// 정렬
-		Arrays.sort(numbers);
-
-		picks = new int[M];
-		isSelected = new boolean[N];
-		permutation(0);
+		Arrays.sort(num);
+		order = new int[M];
+		isVisited = new boolean[N];
+		recursion(0);
 
 		bw.write(sb.toString());
 		bw.flush();
 
 		br.close();
 		bw.close();
-
 	}
 
-	static void permutation(int depth) {
-		// 기저 조건: M개를 모두 선택한 경우
+	static void recursion(int depth) {
 		if (depth == M) {
-			for (int pick : picks)
-				sb.append(pick).append(" ");
+			for (int i = 0; i < M; i++)
+				sb.append(order[i]).append(" ");
 
 			sb.append("\n");
-
 			return;
 		}
 
-		for (int i = 0; i < N; i++) {
-			if (isSelected[i])
+		for(int i = 0; i < N; i++) {
+			if(isVisited[i])
 				continue;
-
-			picks[depth] = numbers[i];
-			isSelected[i] = true;
-			permutation(depth + 1);
-			isSelected[i] = false;
+			
+			order[depth] = num[i];
+			isVisited[i] = true;
+			recursion(depth + 1);
+			isVisited[i] = false;
 		}
 	}
-
 }
