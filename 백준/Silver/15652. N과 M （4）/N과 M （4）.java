98e808a19531da5c0e3350330f1ff7a6a2ee25c5
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-19 15:28:45 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 16952 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -4,22 +4,19 @@ import java.util.*;
 public class Main {
 
 	static int N, M;
-	static int[] picks;
-	static StringBuilder sb;
+	static int[] num;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st;
-		sb = new StringBuilder();
-
-		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		picks = new int[M];
-		permutation(0, 0);
+		num = new int[M];
+		recursion(0, 1);
 
 		bw.write(sb.toString());
 		bw.flush();
@@ -28,20 +25,18 @@ public class Main {
 		bw.close();
 	}
 
-	static void permutation(int depth, int start) {
-		// 기저조건: M개를 모두 선택한 경우
+	static void recursion(int depth, int start) {
 		if (depth == M) {
-			for (int pick : picks)
-				sb.append(pick).append(" ");
+			for (int i = 0; i < M; i++)
+				sb.append(num[i]).append(" ");
 
 			sb.append("\n");
-
 			return;
 		}
 
-		for (int i = start; i < N; i++) {
-			picks[depth] = i + 1;
-			permutation(depth + 1, i);
+		for (int i = start; i <= N; i++) {
+			num[depth] = i;
+			recursion(depth + 1, i);
 		}
 	}
 }
