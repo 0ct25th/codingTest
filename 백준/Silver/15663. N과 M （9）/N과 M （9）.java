6f8fe69c7d267776fc542179836b7967c77ce204
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-19 23:38:12 +0900

    [Silver II] Title: N과 M (9), Time: 172 ms, Memory: 20952 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, M;
+	static int[] num, order;
+	static boolean[] isVisited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		num = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			num[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(num);
+		order = new int[M];
+		isVisited = new boolean[N];
+		recursion(0);
+
+		bw.write(sb.toString());
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+
+	static void recursion(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++)
+				sb.append(order[i]).append(" ");
+
+			sb.append("\n");
+			return;
+		}
+
+		int before = 0;
+		for (int i = 0; i < N; i++) {
+			if (isVisited[i] || before == num[i])
+				continue;
+
+			isVisited[i] = true;
+			order[depth] = num[i];
+			before = num[i];
+			recursion(depth + 1);
+			isVisited[i] = false;
+		}
+	}
+}
