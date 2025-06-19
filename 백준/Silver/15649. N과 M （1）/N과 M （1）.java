4af4106e9aed43eeb3d29865d63ed41e93e585a9
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-19 14:52:45 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,46 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int N, M;
-	static int[] numbers;
-	static boolean[] isSelected;
+
+	static int N, M, num[];
+	static boolean[] isVisited;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine().strip());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
-		numbers = new int[M];
-		isSelected = new boolean[N];
-		permutation(0);
-		
+
+		isVisited = new boolean[N + 1];
+		num = new int[M];
+		recursion(0);
+
+		bw.write(sb.toString());
+		bw.flush();
+
 		br.close();
+		bw.close();
 	}
-	
-	static void permutation(int depth) {
-		// 기저조건: 모든 숫자를 탐방한 경우
-		if(depth == M) {
-			// 완성된 순열 출력
-			for(int number: numbers) 
-				System.out.print(number + " ");
-			System.out.println();
-			
+
+	static void recursion(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++)
+				sb.append(num[i]).append(" ");
+			sb.append("\n");
 			return;
 		}
-		
-		for(int i = 0; i < N; i++) {
-			// 이미 선택한 숫자인 경우
-			if(isSelected[i])
-				continue; // 넘김
-			
-			numbers[depth] = i + 1;		// 완성 순열에 넣기
-			isSelected[i] = true; 	// 선택
-			permutation(depth + 1);		// 재귀
-			isSelected[i] = false; 	// 원복
+
+		for (int i = 1; i <= N; i++) {
+			if (isVisited[i])
+				continue;
+
+			isVisited[i] = true;
+			num[depth] = i;
+			recursion(depth + 1);
+			isVisited[i] = false;
 		}
 	}
 }
