eb32f256a23fd1a956b4f52b3df8ffc85c7a13dd
yoon <72852508+0ct25th@users.noreply.github.com>
2025-06-20 15:17:53 +0900

    [Silver II] Title: 로또, Time: 108 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -2,54 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int N;
-	static int[] input, numbers;
+
+	static int k;
+	static int[] S, n;
 	static StringBuilder sb = new StringBuilder();
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringTokenizer st;
-		
-		while(true) {
-			st = new StringTokenizer(br.readLine().strip());
-			N = Integer.parseInt(st.nextToken());
-			
-			if(N == 0)
+
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+
+			k = Integer.parseInt(st.nextToken());
+			if (k == 0)
 				break;
-			
-			input = new int[N];
-			for(int i = 0; i < N; i++) 
-				input[i] = Integer.parseInt(st.nextToken());
-			
-			numbers = new int[6];
-			combination(0, 0);
+
+			S = new int[k];
+			for (int i = 0; i < k; i++)
+				S[i] = Integer.parseInt(st.nextToken());
+
+			n = new int[6];
+			recursion(0, 0);
 			sb.append("\n");
-			
-		} // end of TestCase
-		
+		}
+
 		bw.write(sb.toString());
 		bw.flush();
-		
+
 		br.close();
 		bw.close();
 	}
-	
-	static void combination(int depth, int start) {
-		// 기저조건: 모든 수를 다 고른 경우
-		if(depth == 6) {
-			for(int number: numbers)
-				sb.append(number).append(" ");
+
+	static void recursion(int depth, int start) {
+		if (depth == 6) {
+			for (int i : n)
+				sb.append(i).append(" ");
+
 			sb.append("\n");
-			
+
 			return;
 		}
-		
-		for(int i = start; i < N; i++) {
-			numbers[depth] = input[i];
-			combination(depth + 1, i + 1);
+
+		for (int i = start; i < k; i++) {
+			n[depth] = S[i];
+			recursion(depth + 1, i + 1);
 		}
 	}
 }
