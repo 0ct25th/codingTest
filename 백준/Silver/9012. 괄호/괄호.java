31a9da3b74294926ffcb60dee101670eca5ad835
yoon <72852508+0ct25th@users.noreply.github.com>
2025-07-07 20:51:50 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,42 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+	static String str;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine().strip());
-		for(int t = 0; t < T; t++) {
-			sb.append(chk(br.readLine()));
-		} // end of TestCase
-		
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+			str = br.readLine();
+
+			sb.append(VPS() ? "YES\n" : "NO\n");
+		}
+
 		bw.write(sb.toString());
 		bw.flush();
-		
+
 		br.close();
 		bw.close();
 	}
-	
-	static String chk(String str) {
-		Stack<Integer> stk = new Stack<>();
-		
-		for (int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == '(')
-				stk.add(0);
+
+	static boolean VPS() {
+		Stack<Character> stk = new Stack<>();
+
+		for (char ch : str.toCharArray()) {
+			if (ch == '(')
+				stk.add('(');
 			else {
 				if (stk.isEmpty())
-					return "NO\n";
-				
+					return false;
+
 				stk.pop();
 			}
 		}
-		
-		// stk에 괄호가 남은 경우
-		if(!stk.isEmpty())
-			return "NO\n";
-		
-		return "YES\n";
+
+		if (!stk.isEmpty())
+			return false;
+
+		return true;
 	}
 }
