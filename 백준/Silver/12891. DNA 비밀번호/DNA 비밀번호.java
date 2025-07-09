659872277126e51161d957d3b97cbb6326c0df74
yoon <72852508+0ct25th@users.noreply.github.com>
2025-07-09 22:25:19 +0900

    [Silver II] Title: DNA 비밀번호, Time: 264 ms, Memory: 20224 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -3,78 +3,83 @@ import java.util.*;
 
 public class Main {
 
-	static int S, P, pwd[], cnt[];
-	static long result;
+	static int S, P;
 	static String dna;
+	static int[] str, use;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// 첫 번째 줄에 민호가 임의로 만든 DNA 문자열 길이 |S|와 비밀번호로 사용할 부분문자열의 길이 |P| 가 주어진다.
-		st = new StringTokenizer(br.readLine());
-		S = Integer.parseInt(st.nextToken());
-		P = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken()); // DNA 문자열 길이
+		P = Integer.parseInt(st.nextToken()); // 비밀번호로 사용할 부분 문자열의 길이
 
-		// 두번 째 줄에는 민호가 임의로 만든 DNA 문자열이 주어진다.
 		dna = br.readLine();
 
-		// 세번 째 줄에는 부분문자열에 포함되어야 할 {‘A’, ‘C’, ‘G’, ‘T’} 의 최소 개수가 공백을 구분으로 주어진다.
-		pwd = new int[4];
+		str = new int[4];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < 4; i++)
-			pwd[i] = Integer.parseInt(st.nextToken());
+			str[i] = Integer.parseInt(st.nextToken());
 
-		//////////////// end of Input
-
-		slidingWindow();
-
-		System.out.println(result);
+		System.out.println(slidingWindow());
 	}
 
-	static void slidingWindow() {
-		cnt = new int[4];
+	static int slidingWindow() {
+		use = new int[4];
+		int result = 0;
 
-		// 첫번째 윈도우 생성
 		for (int i = 0; i < P; i++) {
 			char ch = dna.charAt(i);
 
-			cnt[idx(ch)]++;
+			if (ch == 'A')
+				use[0]++;
+			else if (ch == 'C')
+				use[1]++;
+			else if (ch == 'G')
+				use[2]++;
+			else
+				use[3]++;
 		}
 
-		if (isChk())
+		if (password())
 			result++;
 
-		for (int i = 1; i < S - P + 1; i++) {
-			// 왼쪽 몰아내기
-			cnt[idx(dna.charAt(i - 1))]--;
-			// 오른쪽 들여오기
-			cnt[idx(dna.charAt(i + P - 1))]++;
+		for (int i = 1; i <= S - P; i++) {
+			// 왼쪽 제거
+			char before = dna.charAt(i - 1);
+			if (before == 'A')
+				use[0]--;
+			else if (before == 'C')
+				use[1]--;
+			else if (before == 'G')
+				use[2]--;
+			else
+				use[3]--;
 
-			if (isChk())
+			// 오른쪽 추가
+			char after = dna.charAt(i + P - 1);
+			if (after == 'A')
+				use[0]++;
+			else if (after == 'C')
+				use[1]++;
+			else if (after == 'G')
+				use[2]++;
+			else
+				use[3]++;
+
+			if (password())
 				result++;
 		}
+
+		return result;
 	}
 
-	static boolean isChk() {
-		for (int i = 0; i < 4; i++)
-			if (pwd[i] > cnt[i])
+	static boolean password() {
+		for (int i = 0; i < 4; i++) {
+			if (use[i] < str[i])
 				return false;
+		}
 
 		return true;
-	}
-
-	static int idx(char ch) {
-		switch (ch) {
-		case 'A':
-			return 0;
-		case 'C':
-			return 1;
-		case 'G':
-			return 2;
-		case 'T':
-			return 3;
-		}
-		return 4;
 	}
 }
