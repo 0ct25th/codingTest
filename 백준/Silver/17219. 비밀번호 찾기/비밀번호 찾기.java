2c00b95d20d392b16800d4cccf7c9fcf1417e36a
yoon <72852508+0ct25th@users.noreply.github.com>
2025-07-15 22:13:34 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 552 ms, Memory: 56740 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -4,31 +4,36 @@ import java.util.*;
 public class Main {
 
 	static int N, M;
-	static Map<String, String> map;
-
+	static Map<String, String> hash;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		st = new StringTokenizer(br.readLine());
-		// 저장된 사이트 주소의 수 N
-		N = Integer.parseInt(st.nextToken());
-		// 비밀번호를 찾으려는 사이트 주소의 수 M
-		M = Integer.parseInt(st.nextToken());
-
-		map = new HashMap<>();
-		for (int n = 0; n < N; n++) {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken()); // 저장된 주소의 수
+		M = Integer.parseInt(st.nextToken()); // 찾으려는 주소의 수
+		
+		hash = new HashMap<>();
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			String url = st.nextToken();
+			String site = st.nextToken();
 			String pwd = st.nextToken();
-
-			map.put(url, pwd);
+			
+			hash.put(site, pwd);
 		}
-
-		for (int m = 0; m < M; m++) {
-			String url = br.readLine();
-
-			System.out.println(map.get(url));
+		
+		for(int i = 0; i < M; i++) {
+			String site = br.readLine();
+			
+			sb.append(hash.get(site)).append("\n");
 		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		
+		br.close();
+		bw.close();
 	}
 }
