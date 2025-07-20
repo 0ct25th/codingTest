fc70e4d197cf886fc7f8455d0d887096ff6d7bca
yoon <72852508+0ct25th@users.noreply.github.com>
2025-07-20 14:10:15 +0900

    [Silver I] Title: 절댓값 힙, Time: 296 ms, Memory: 25180 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,54 +1,56 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	static Queue<Num> pq = new PriorityQueue<>();
-	
+	static int N;
+	static Queue<Num> pq;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine().strip());
-		for(int t = 0; t < T; t++) {
+
+		N = Integer.parseInt(br.readLine());
+
+		pq = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
-			
-			if(x == 0) {
-				if(!pq.isEmpty())
-					sb.append(pq.poll().origin).append("\n");
-				else
-					sb.append("0\n");
-			}
-			else
+
+			if (x != 0)
 				pq.offer(new Num(x, Math.abs(x)));
-			
-		} // end of TestCase
-		
+			else {
+				if (pq.isEmpty())
+					sb.append("0\n");
+				else {
+					Num cur = pq.poll();
+
+					sb.append(cur.org).append("\n");
+				}
+			}
+		}
+
 		bw.write(sb.toString());
 		bw.flush();
-		
+
 		br.close();
 		bw.close();
-	} // end of main
-	
+	}
+
 	static class Num implements Comparable<Num> {
-		int origin;
-		int abs;
-		
-		Num(int origin, int abs) {
-			this.origin = origin;
+		int org, abs;
+
+		Num(int org, int abs) {
+			this.org = org;
 			this.abs = abs;
 		}
-		
+
 		@Override
 		public int compareTo(Num o) {
 			if (this.abs == o.abs)
-				return (this.origin - o.origin);
-			
-			return (this.abs - o.abs);
+				return Integer.compare(this.org, o.org);
+
+			return Integer.compare(this.abs, o.abs);
 		}
 	}
 }
