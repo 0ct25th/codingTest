d2f5e59c19c524687d9158ba6b270bb210bfbd22
yoon <72852508+0ct25th@users.noreply.github.com>
2025-07-22 21:25:59 +0900

    [Gold V] Title: 트리, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
 
-	static int N, parent[], M, start, result;
-	static boolean isVisited[];
-	static List<Integer>[] nodeList;
+	static int N, start, remove;
+	static List<Integer>[] adjList;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,67 +12,54 @@ public class Main {
 
 		N = Integer.parseInt(br.readLine());
 
-		nodeList = new ArrayList[N];
+		adjList = new ArrayList[N];
 		for (int i = 0; i < N; i++)
-			nodeList[i] = new ArrayList<>();
-
-		parent = new int[N];
+			adjList[i] = new ArrayList<>();
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			int p = Integer.parseInt(st.nextToken());
-			parent[i] = p;
 
-			// 부모가 없는 루트인 경우
-			if (p == -1) {
+			if (p == -1)
 				start = i;
-				continue;
-			}
-
-			nodeList[p].add(i);
+			else
+				adjList[p].add(i);
 		}
 
-		// 지울 노드 번호
-		M = Integer.parseInt(br.readLine());
+		remove = Integer.parseInt(br.readLine());
+		adjList[remove] = new ArrayList<>();
 
-		///////////////////// end of Input
-		
-		// 지울 노드의 부모 자식 리스트에서 지우기
-		if(parent[M] == -1)
-			nodeList[M].clear();
-		else {
-			int p = parent[M];
-			for(int i = 0; i < nodeList[p].size(); i++) {
-				if(nodeList[p].get(i) != M)
-					continue;
-				
-				nodeList[p].remove(i);
-				break;
-			}
-			
-			isVisited = new boolean[N];
-			dfs(start);
-		}
-
+		int result = bfs(start);
 		System.out.println(result);
 	}
 
-	static void dfs(int cur) {
-		isVisited[cur] = true;
-		
-		// 현재 노드가 리프 노드인 경우
-		if(nodeList[cur].size() == 0)
-			result++;
+	static int bfs(int start) {
+		Queue<Integer> dq = new ArrayDeque<>();
+		boolean[] isVisited = new boolean[N];
+		int cnt = 0;
 
-		// 현재 노드의 자식이 있는 경우
-		for (int nxt : nodeList[cur]) {
-			if (nxt == M || isVisited[nxt])
+		isVisited[start] = true;
+		dq.offer(start);
+
+		while (!dq.isEmpty()) {
+			int cur = dq.poll();
+
+			if (cur == remove)
 				continue;
+			else if (adjList[cur].size() == 0)
+				cnt++;
+			else if (adjList[cur].size() == 1 && adjList[cur].get(0) == remove)
+				cnt++;
+			else {
+				for (int nxt : adjList[cur]) {
+					if (isVisited[nxt] || nxt == remove)
+						continue;
 
-			// 연결된 노드(nxt)가 리프 노드인 경우
-			if (nodeList[nxt].size() == 0)
-				result++;
-			else
-				dfs(nxt);
+					isVisited[nxt] = true;
+					dq.offer(nxt);
+				}
+			}
 		}
+
+		return cnt;
 	}
 }
