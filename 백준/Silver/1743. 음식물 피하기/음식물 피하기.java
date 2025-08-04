42ad8df393d5412370f19ba122ceb4f024c7bce7
yoon <72852508+0ct25th@users.noreply.github.com>
2025-08-04 20:54:54 +0900

    [Silver I] Title: 음식물 피하기, Time: 172 ms, Memory: 15524 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -4,44 +4,45 @@ import java.util.*;
 public class Main {
 
 	static int N, M, K, result;
-	static boolean[][] trash, isVisited;
-
+	static int[][] map;
+	static boolean[][] isVisited;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		trash = new boolean[N + 1][M + 1];
-		for (int k = 0; k < K; k++) {
+		map = new int[N + 1][M + 1];
+		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 
-			trash[r][c] = true; // 쓰레기
+			map[r][c] = 1;
 		}
-		///////////////////////////// end of Input
 
-		result = 0;
 		isVisited = new boolean[N + 1][M + 1];
-		for (int r = 1; r < N + 1; r++)
-			for (int c = 1; c < M + 1; c++)
-				// 쓰레기 && 방문하지 않은 쓰레기
-				if (trash[r][c] && !isVisited[r][c])
-					result = Math.max(result, bfs(r, c));
+		for (int r = 1; r <= N; r++) {
+			for (int c = 1; c <= M; c++) {
+				if (isVisited[r][c] || map[r][c] == 0)
+					continue;
 
+				result = Math.max(result, bfs(r, c));
+			}
+		}
+		
 		System.out.println(result);
 	}
 
 	static int bfs(int sr, int sc) {
-		int count = 1;
 		Queue<Coord> dq = new ArrayDeque<>();
+		int size = 1;
+
 		isVisited[sr][sc] = true;
 		dq.offer(new Coord(sr, sc));
 
@@ -50,31 +51,28 @@ public class Main {
 			int r = cur.r;
 			int c = cur.c;
 
-			// 4방향 탐색
 			for (int d = 0; d < 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
 
-				// 유효범위 벗어남 || 이미 방문 || 쓰레기 아닌 경우
-				if (!isValidCoord(nr, nc) || isVisited[nr][nc] || !trash[nr][nc])
-					continue; // 넘김
+				if (!isValidCoord(nr, nc) || isVisited[nr][nc] || map[nr][nc] == 0)
+					continue;
 
+				size++;
 				isVisited[nr][nc] = true;
-				count++; // 개수 카운트
 				dq.offer(new Coord(nr, nc));
 			}
 		}
 
-		return count;
+		return size;
 	}
 
 	static boolean isValidCoord(int r, int c) {
-		return 0 < r && r < N + 1 && 0 < c && c < M + 1;
+		return 0 < r && r <= N && 0 < c && c <= M;
 	}
 
 	static class Coord {
-		int r;
-		int c;
+		int r, c;
 
 		Coord(int r, int c) {
 			this.r = r;
