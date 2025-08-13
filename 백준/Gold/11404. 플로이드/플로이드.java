ad0fc49372e202cc0ce2c989bb2610d805669a68
yoon <72852508+0ct25th@users.noreply.github.com>
2025-08-13 21:55:50 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 42380 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Main {
 
-	static final int INF = 10000001;
+	static final int INF = 1_000_000_000;
+
 	static int n, m;
 	static int[][] map;
 
@@ -12,45 +13,44 @@ public class Main {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		n = Integer.parseInt(br.readLine()); // 도시 개수
-		m = Integer.parseInt(br.readLine()); // 버스 개수
+		n = Integer.parseInt(br.readLine()); // 도시의 개수
+		m = Integer.parseInt(br.readLine()); // 버스의 개수
 
 		map = new int[n + 1][n + 1];
-		for (int i = 1; i <= n; i++)
+
+		// 초기화: 자기 자신 0, 나머지 INF
+		for (int i = 1; i <= n; i++) {
 			Arrays.fill(map[i], INF);
+			map[i][i] = 0;
+		}
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()); // 시작 도시
-			int b = Integer.parseInt(st.nextToken()); // 도착 도시
-			int c = Integer.parseInt(st.nextToken()); // 필요 비용
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-			// 시작 도시와 도착 도시를 연결하는 노선은 하나가 아닐 수 있다.
-			if (map[a][b] > c)
-				map[a][b] = c;
+			map[a][b] = Math.min(map[a][b], c);
 		}
 
-		floydWarshall();
-
-		for (int r = 1; r <= n; r++) {
-			for (int c = 1; c <= n; c++) {
-				if (map[r][c] >= INF || r == c)
-					map[r][c] = 0;
-
-				sb.append(map[r][c]).append(" ");
-			}
-			sb.append("\n");
-		}
-
-		System.out.println(sb);
-	}
-
-	static void floydWarshall() {
 		for (int k = 1; k <= n; k++) {
 			for (int i = 1; i <= n; i++) {
-				for (int j = 1; j <= n; j++)
-					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+				for (int j = 1; j <= n; j++) {
+					if (map[k][j] == INF || map[i][j] <= map[i][k] + map[k][j])
+						continue;
+
+					map[i][j] = map[i][k] + map[k][j];
+				}
 			}
 		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++)
+				sb.append(map[i][j] == INF ? 0 : map[i][j]).append(' ');
+
+			sb.append('\n');
+		}
+
+		System.out.print(sb);
 	}
 }
