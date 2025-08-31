0b99bd4882f2ff12c799c1c94b6a39d07b059fdf
yoon <72852508+0ct25th@users.noreply.github.com>
2025-08-31 20:39:07 +0900

    [Gold V] Title: 경쟁적 전염, Time: 256 ms, Memory: 22248 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -5,29 +5,24 @@ public class Main {
 
 	static int N, K, S, X, Y;
 	static int[][] map;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static Queue<Coord> pq;
+	static List<Coord> list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
 		map = new int[N + 1][N + 1];
-		pq = new PriorityQueue<>();
+		list = new LinkedList<>();
 		for (int r = 1; r <= N; r++) {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 1; c <= N; c++) {
 				map[r][c] = Integer.parseInt(st.nextToken());
 
-				if (map[r][c] == 0)
-					continue;
-
-				pq.offer(new Coord(r, c, map[r][c], 0));
+				if (map[r][c] != 0)
+					list.add(new Coord(map[r][c], r, c));
 			}
 		}
 
@@ -37,57 +32,51 @@ public class Main {
 		Y = Integer.parseInt(st.nextToken());
 
 		bfs();
-
+		
 		System.out.println(map[X][Y]);
-
 	}
 
 	static void bfs() {
-		while (!pq.isEmpty()) {
-			Coord cur = pq.poll();
-			int r = cur.r;
-			int c = cur.c;
-			int cnt = cur.cnt;
+		int[] dr = { -1, 1, 0, 0 };
+		int[] dc = { 0, 0, -1, 1 };
 
-			if (r == X && c == Y)
-				return;
+		while (S > 0 && !list.isEmpty()) {
+			S--;
+			Collections.sort(list, (o1, o2) -> Integer.compare(o1.n, o2.n));
+			int size = list.size();
 
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
+			for (int i = 0; i < size; i++) {
+				Coord cur = list.remove(0);
+				int n = cur.n;
+				int r = cur.r;
+				int c = cur.c;
 
-				if (!isValidCoord(nr, nc) || map[nr][nc] != 0 || cnt + 1 > S)
-					continue;
+				for (int d = 0; d < 4; d++) {
+					int nr = r + dr[d];
+					int nc = c + dc[d];
 
-				map[nr][nc] = map[r][c];
-				pq.offer(new Coord(nr, nc, map[nr][nc], cnt + 1));
+					if (!isValidCoord(nr, nc) || map[nr][nc] != 0)
+						continue;
+
+					map[nr][nc] = n;
+					list.add(new Coord(n, nr, nc));
+				}
 			}
 		}
 	}
-
+	
 	static boolean isValidCoord(int r, int c) {
 		return 0 < r && r <= N && 0 < c && c <= N;
 	}
 
-	static class Coord implements Comparable<Coord> {
-		int r, c;
-		int num;
-		int cnt;
+	static class Coord {
+		int n; // 번호
+		int r, c; // 좌표
 
-		Coord(int r, int c, int num, int cnt) {
+		Coord(int n, int r, int c) {
+			this.n = n;
 			this.r = r;
 			this.c = c;
-			this.num = num;
-			this.cnt = cnt;
 		}
-
-		@Override
-		public int compareTo(Coord o) {
-			if (this.cnt == o.cnt)
-				return (this.num - o.num);
-
-			return (this.cnt - o.cnt);
-		}
-
 	}
 }
